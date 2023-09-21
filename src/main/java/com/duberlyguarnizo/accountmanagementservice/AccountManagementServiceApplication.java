f10fc8334816e31a5d86f13fb3c4873529c35a0c
Duberly, Guarnizo Fuentes Rivera <duberlygfr@gmail.com>
2023-09-21 13:44:52 -0500

    Initial Commit

---
+++ src/main/java/com/duberlyguarnizo/accountmanagementservice/AccountManagementServiceApplication.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2023. Code by Duberly Guarnizo <duberlygfr@gmail.com>.
+ */
+
+package com.duberlyguarnizo.accountmanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class AccountManagementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountManagementServiceApplication.class, args);
+    }
+
+}
